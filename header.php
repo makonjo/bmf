993c074055c935e08af008ae8dad03971cba3002
Otto Akama <akamaotto@gmail.com>
2016-05-09 11:24:31 +0100

    some small changes

---
+++ header.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Home: BMF - Buea Municipal Council’s Business Management and Public Service Integrity Fellowship</title>
+<meta name="viewport" content="width=device-width, maximum-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href="style/common.css" rel="stylesheet">
+<link href="style/style.css" rel="stylesheet">
+<link href="style/camera.css" rel="stylesheet">
+<link href="style/slider.css" rel="stylesheet">
+<link href="style/responsive.css" rel="stylesheet">
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
+
+</head>
+
+<body>
+<div class="wrap">
+  <div class="tp-bar">
+    <div class="container">
+      <div class="logo"> <a href="index-2.html"><img src="images/logo.png" alt="BMF"></a> <span class="slogan"> Buea Municipal Council’s Business Management and Public Service Integrity Fellowship  </span> </div>
+      <!-- logo ends -->
+      <div class="tp-right">
+        <div class="social-links">
+          <ul>
+            <li><a href="http://facebook.com/skademy" class="icon-facebook tooltip" title="facebook"></a></li>
+            <li><a href="http://skademy.com" class="icon-rss tooltip" title="rss"></a></li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  </div><!-- tp-bar ends -->
+  <nav class="menu-bar">
+    <div class="container">
+      <a href="#" class="tablet-menu"></a>
+      <ul class="menu">
+        <li><a href="index-2.html">Home</a></li>
+        <li><a href="events.html">Events</a></li>
+        <li><a href="blog.html">Blog</a></li>
+        <li>
+          <a href="gallery.html">Gallery</a>
+          <ul>
+            <li><a href="gallery-2col.html">two colum</a></li>
+            <li><a href="gallery.html">three colum</a></li>
+            <li><a href="gallery-4col.html">four colum</a></li>
+          </ul>
+        </li>
+        <li><a href="contact.html">Contact Us</a></li>
+        <li>
+          <a href="#">Pages</a>
+          <ul>
+            <li>
+              <a href="events.html">Events</a>
+              <ul>
+                <li><a href="events-detail.html">Event Detail</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="blog.html">Blog</a>
+              <ul>
+                <li><a href="blog-detail.html">Blog Detail</a></li>
+              </ul>
+            </li>
+            <li>
+              <a href="gallery.html">Gallery</a>
+              <ul>
+                <li><a href="gallery-2col.html">two colum</a></li>
+                <li><a href="gallery.html">three colum</a></li>
+                <li><a href="gallery-4col.html">four colum</a></li>
+              </ul>
+            </li>
+            <li><a href="contact.html">Contact Us</a></li>
+            <li>
+              <a href="#">Cursus Tempor</a>
+              <ul>
+                <li><a href="#">Ultrices augue dolor</a></li>
+                <li>
+                  <a href="#">Etiam risus et</a>
+                  <ul>
+                    <li><a href="#">Etiam risus et</a></li>
+                    <li><a href="#">Ultrices augue dolor</a></li>
+                    <li><a href="#">Cursus Tempor</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul><!-- menu ends --> 
+      <div class="user-controls">
+        <a href="#" class="user-login"></a>
+        <a href="#" class="user-search"></a>
+      </div>
+    </div>
+  </nav><!-- menu-bar ends -->
